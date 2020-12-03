215fc65b17a2e2e3e61e0fd67b77fd8e6db92179
Leober Ramos <leober.ramos33@gmail.com>
2020-12-03 00:13:11 -0400

    Add Prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,79 +1,79 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 const config = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'js/dist/[name].js',
-    publicPath: ''
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/dist/[name].js",
+    publicPath: "",
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        use: 'babel-loader',
-        exclude: /node_modules/
+        use: "babel-loader",
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/,
         use: [
           MiniCssExtractPlugin.loader,
           {
-            loader: 'css-loader',
+            loader: "css-loader",
             options: {
-              importLoaders: 1
-            }
+              importLoaders: 1,
+            },
           },
-          'postcss-loader'
-        ]
+          "postcss-loader",
+        ],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        loader: 'file-loader',
+        loader: "file-loader",
         options: {
-          outputPath: '../../img',
-          name: '[name].[ext]'
-        }
-      }
-    ]
+          outputPath: "../../img",
+          name: "[name].[ext]",
+        },
+      },
+    ],
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'src/views/index.html',
-      inject: false
+      filename: "index.html",
+      template: "src/views/index.html",
+      inject: false,
     }),
     new MiniCssExtractPlugin({
-      filename: 'css/dist/style.css'
+      filename: "css/dist/style.css",
     }),
     new OptimizeCssAssetsPlugin({}),
     new CopyPlugin({
       patterns: [
         {
-          from: 'public',
-          to: '.'
-        }
-      ]
-    })
+          from: "public",
+          to: ".",
+        },
+      ],
+    }),
   ],
   optimization: {
-    runtimeChunk: 'single',
+    runtimeChunk: "single",
     splitChunks: {
       cacheGroups: {
         vendor: {
           test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all'
-        }
-      }
+          name: "vendors",
+          chunks: "all",
+        },
+      },
     },
-  }
-}
+  },
+};
 
-module.exports = config
+module.exports = config;
