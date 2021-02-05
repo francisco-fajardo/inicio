0bc0a87bd7350bb6dcbc53c76c6ad334e6daa8f4
Leober Ramos <leober.ramos33@gmail.com>
2021-02-05 00:08:04 -0400

    Use full path instead of relative path in URLs

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "js/dist/[name].js",
-        publicPath: "",
+        publicPath: "/",
     },
     module: {
         rules: [
